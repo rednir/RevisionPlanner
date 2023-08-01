6fd855199d284d536415b44f323f4ddf42ec010a
Davran Dilshat <davran.dilshat@gmail.com>
2023-08-01 16:44:08 +0100

    use dependency injection

--- RevisionPlanner/MauiProgram.cs
+++ RevisionPlanner/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using RevisionPlanner.Model;
+using RevisionPlanner.View;
+using RevisionPlanner.ViewModel;
+using RevisionPlanner.ViewModel.Setup;
 
 namespace RevisionPlanner;
 
@@ -13,7 +17,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.AddServices();
 
 #if DEBUG
 		builder.Logging.AddDebug();
@@ -21,5 +26,28 @@ public static class MauiProgram
 
 		return builder.Build();
 	}
+
+	/// <summary>
+	/// Register services into the application dependency injection container. These services are resolved throughout the application.
+	/// </summary>
+	private static MauiAppBuilder AddServices(this MauiAppBuilder builder)
+	{
+		// Register database services.
+		builder.Services.AddSingleton<UserDatabase>();
+
+		// Register view models.
+		builder.Services.AddSingleton<TimetableTodayViewModel>();
+		builder.Services.AddTransient<SelectQualificationViewModel>();
+		builder.Services.AddTransient<SelectStudyDaysViewModel>();
+		builder.Services.AddTransient<SelectSubjectsViewModel>();
+
+		// Register views.
+		builder.Services.AddSingleton<TimetableTodayPage>();
+		builder.Services.AddTransient<SelectQualificationPage>();
+		builder.Services.AddTransient<SelectStudyDaysPage>();
+		builder.Services.AddTransient<SelectSubjectsPage>();
+
+		return builder;
+    }
 }
 
