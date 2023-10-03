c80546a88747ecadeed139bf727488f187357323
Davran Dilshat <davran.dilshat@gmail.com>
2023-10-03 18:03:58 +0100

    add checkboxes for study days

--- RevisionPlanner/ViewModel/Setup/SelectStudyDaysViewModel.cs
+++ RevisionPlanner/ViewModel/Setup/SelectStudyDaysViewModel.cs
@@ -5,6 +5,20 @@ namespace RevisionPlanner.ViewModel.Setup;
 
 public class SelectStudyDaysViewModel : ViewModelBase
 {
+    public bool Monday { get; set; } = true;
+
+    public bool Tuesday { get; set; } = true;
+
+    public bool Wednesday { get; set; } = true;
+
+    public bool Thursday { get; set; } = true;
+
+    public bool Friday { get; set; } = true;
+
+    public bool Saturday { get; set; }
+
+    public bool Sunday { get; set; }
+
     public ICommand NextCommand { get; private set; }
 
     public SelectStudyDaysViewModel(Action next)
